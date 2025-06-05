7d48a7882cf3f16ed8eb420f4e3e9e7211ac64aa
nickooo420 <n.frantz@edu.pucrs.br>
2025-06-04 23:29:29 -0300

    test deploy.yml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import TopAnime from "@/components/TopAnime"
 export default function Home(){
     return(<div className="h-screen text-[#A7ADBB]">
-                <h1 className="text-center">Top Anime of All Time!!</h1>
+                <h1 className="text-center">Top Anime of All Time!</h1>
                 <TopAnime/>
             </div>
     )
